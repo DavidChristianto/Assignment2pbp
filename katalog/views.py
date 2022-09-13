138c75a84224cc287ef47ce16796e2bbcf40bbb2
DavidChristianto <david.christianto@ui.ac.id>
2022-09-13 17:23:06 +0700

    Hello

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'David'
+    }
+    return render(request, "katalog.html", context)
